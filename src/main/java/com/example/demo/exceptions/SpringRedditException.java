17ff490d67604a5b7254cb7ed17b6998172c3388
northwind1337 <brytong@ukr.net>
2022-06-28 14:02:26 +0300

    v 0.2 - start working with Exception Handling Mistakes

--- src/main/java/com/example/demo/exceptions/SpringRedditException.java
+++ src/main/java/com/example/demo/exceptions/SpringRedditException.java
@@ -1,2 +1,11 @@
-package com.example.demo.exceptions;public class SpringRedditException {
+package com.example.demo.exceptions;
+
+public class SpringRedditException extends RuntimeException {
+    public SpringRedditException(String exMessage, Exception exception) {
+        super(exMessage, exception);
+    }
+
+    public SpringRedditException(String exMessage) {
+        super(exMessage);
+    }
 }
