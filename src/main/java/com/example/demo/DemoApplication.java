b17a290bc68f71f4026eb644e4d88ba88a0cdf8a
northwind1337 <brytong@ukr.net>
2022-06-28 14:58:16 +0300

    v 0.4 - created frotend using angular

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import com.programming.techie.springredditclone.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@Import(SwaggerConfiguration.class)
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(com.example.demo.DemoApplication.class, args);
+  }
 }
